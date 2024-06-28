261ccdf5eabdf91dfa9615e44f1a09b41ff630fe
Sudhan Kandel <sujan.kandel.0ne8@gmail.com>
2024-06-28 18:42:42 +0545

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from "react";
+// import Ratings from "./components/Rating/Ratings";
+// import Counter from "./components/Counter/Counter";
+// import BgChanger from "./components/BgChanger/BgChanger";
+// import PasswordManager from "./components/Password/Password.jsx";
+// import CurrencyX from "./components/Currency/Currency.jsx";
+import RouterPractice from "./components/RoutePractice/components/Home.jsx";
+import "./App.css";
+
+
+function App() {
+ 
+  return (
+    <>
+      {/* <Ratings /> */}
+      {/* <Counter /> */}
+      {/* <BgChanger /> */}
+      {/* <PasswordManager /> */}
+      {/* <CurrencyX /> */}
+     
+   
+    </>
+  );
+}
+
+export default App;
